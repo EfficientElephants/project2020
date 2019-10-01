7dc3c99a0efaa27d9fb8120167e2322cfae5a295
Anna Peterson <petean09@luther.edu>
2019-10-01 14:42:46 -0500

    update

--- backend/server.js
+++ backend/server.js
@@ -87,17 +87,5 @@ router.post('/putData', (req, res) => {
 app.use('/api', router);
 
 
-
-if (process.env.NODE_ENV === 'production') {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-// Handle React routing, return all requests to React app
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
-
-
-
 // launch our backend into a port
 app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
