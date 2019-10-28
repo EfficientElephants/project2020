0985236251d8ff6d0e1d22c859366d821bed723e
kuehma02 <kuehma02@luther.edu>
2019-10-27 22:49:53 -0500

    update and create done

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,16 +7,16 @@ router.get('/users', function(req, res) {
   userService.get(req,res);
 });
 
-router.post('/hero', function(req, res) {
-  heroesService.create(req, res);
+router.post('/user', function(req, res) {
+  userService.create(req, res);
 });
 
-// router.put('/hero', function(req, res) {
-//   heroesService.update(req, res);
+// router.put('/user', function(req, res) {
+//   userService.update(req, res);
 // });
 
-// router.delete('/hero/:id', function(req, res) {
-//   heroesService.destroy(req, res);
+// router.delete('/user/:id', function(req, res) {
+//   userService.destroy(req, res);
 // });
 
 module.exports = router;
