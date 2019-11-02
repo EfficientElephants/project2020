80348bae21c0ac80f621e4386ff9e7e4ea232cfd
Anna Peterson <petean09@luther.edu>
2019-11-02 14:17:51 -0500

    navbar working

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,15 +16,18 @@ class NavBar extends Component {
     render() {
         return (
             <div>
-                <Navbar bg="dark" variant="dark">
-                    <Navbar.Brand href="#home">Expense Elephant</Navbar.Brand>
-                    <Nav className="mr-auto">
-                        <Nav.Link><Link to="/">Dashboard</Link></Nav.Link>
-                        <Nav.Link><Link to="/transaction">Transactions</Link></Nav.Link>
-                        <Nav.Link><Link to="/income-mgr">Income Manager</Link></Nav.Link>
-                        <Nav.Link><Link to="/goal-mgr">Goal Manager</Link></Nav.Link>
+                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                    <Navbar.Brand>Expense Elephant</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="mr-auto justify-content-end" defaultActiveKey="/">
+                        <Link to="/">Dashboard</Link>
+                        <Link to="/transaction">Transactions</Link>
+                        <Link to="/income-mgr">Income Manager</Link>
+                        <Link to="/goal-mgr">Goal Manager</Link>
                         {/* <Nav.Link><Link to="/login">Login</Link></Nav.Link> */}
                     </Nav>
+                    </Navbar.Collapse>
                 </Navbar>
 
                 <Switch>
