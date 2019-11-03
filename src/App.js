53e4da63a90b0956ad86f04422d5980c5dcfb273
Anna Peterson <petean09@luther.edu>
2019-11-03 15:46:46 -0600

    possible error fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 //import logo from './logo.svg';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 import Login from './components/Login';
 import { ProtectedRoute } from "./components/ProtectedRoute";
@@ -12,11 +12,9 @@ class App extends Component {
   render() {
     return (  
       <div>
-        <Switch>
           <Route exact path="/" component={ Login }/>
           <ProtectedRoute exact path="/navbar" component={ NavBar }/>
           {/* <Route path="*" compnent={() => "404 NOT FOUND"}/> */}
-        </Switch>
       </div>
 
     );
