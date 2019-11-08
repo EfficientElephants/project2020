362b3fb5a5192898495a6edb9bcf6877b0c101f1
Anna Ruble <rublan01@annas-mbp.lc.luther.edu>
2019-11-07 22:44:22 -0600

    MODAL WORKSSSSS :)

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,52 @@
 import React, { Component } from 'react';
-import { Row, Col, Container } from 'react-bootstrap';
+import { Row, Col, Container, Button, ButtonToolbar, Modal } from 'react-bootstrap';
 import NavBar from './Navbar';
+
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
+function App() {
+  const [modalShow, setModalShow] = React.useState(false);
+
+  return (
+    <ButtonToolbar>
+      <Button variant="primary" onClick={() => setModalShow(true)}>
+        Launch vertically centered modal
+      </Button>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </ButtonToolbar>
+  );
+}
+
 
 class Dashboard extends Component {
 
@@ -21,6 +67,11 @@ class Dashboard extends Component {
                   </Col>
                   <Col>
                     <p>An explanation of spending status will go here too.</p>
+                  </Col>
+                  <Col>
+                    <Button>
+                      <App />
+                    </Button>
                   </Col>
                 </Row>
             </Col>
