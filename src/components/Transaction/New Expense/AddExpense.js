675d0dcd5dbe66d602c1d83021e8147904f88e6e
kuehma02 <kuehma02@luther.edu>
2019-11-23 11:30:08 -0600

    Add modal, everything but validation

--- src/components/Transaction/New Expense/AddExpense.js
+++ src/components/Transaction/New Expense/AddExpense.js
@@ -3,46 +3,78 @@ import { Container, Row, Button } from 'react-bootstrap';
 
 //import purchaseAPI from '../../../api/purchaseAPI';
 import AddExpenseModal from './AddExpenseModal';
+import purchaseAPI from '../../../api/purchaseAPI';
 
 class AddExpense extends Component {
     constructor() {
         super();
         this.state = {
             expenses: [],
-            addingExpense: false,
-            error: false,
+            errors: false,
             showModal: false
         };
 
         this.handleSave = this.handleSave.bind(this);
-        //this.handleCancel = this.handleCancel.bind(this);
-        //this.handleChange = this.handleChange.bind(this);
+        this.handleCancel = this.handleCancel.bind(this);
+        this.handleChange = this.handleChange.bind(this);
         this.handleEnableModal = this.handleEnableModal.bind(this);
         this.handleDisableModal = this.handleDisableModal.bind(this);
     }
 
+    handleChange(event) {
+        let selectedExpense = this.state.selectedExpense;
+        selectedExpense[event.target.name] = event.target.value;
+        this.setState({ selectedExpense: selectedExpense });
+
+    }
+
+    handleCancel() {
+        this.setState({ selectedExpense: null, showModal: false });
+        this.handleDisableModal();
+
+    }
 
     handleEnableModal () {
         this.setState({
             showModal: true,
-            selectedExpense: {item: '', price:'', category: 'Rent'}
+            selectedExpense: {item: '', price:'', category: 'Social'}
         });
         console.log("enabling");
         console.log(this.state.showModal);
+        
     }
 
     handleDisableModal() {
         console.log("disabling");
         this.setState({
-            showModal: false
+            showModal: false,
+            selectedExpense: null
         })
     }
 
     handleSave() {
-        this.handleDisableModal();
+        purchaseAPI
+            .create(this.state.selectedExpense)
+            .then(result => {
+                if (result.errors) {
+                    console.log(result);
+                    this.setState({error: true});
+
+                }
+                else {
+                    console.log('Successfully created!');
+                    this.setState({
+                        selectedExpense: null,
+                        errors: false
+                    });
+                    this.handleDisableModal();
+                }
+            })
+        
     }
 
     render() {
+        console.log(this.state.selectedExpense);
         return (
             <Container>
                 <Row>
@@ -51,7 +83,10 @@ class AddExpense extends Component {
                         <AddExpenseModal 
                             show={this.state.showModal}
                             onHide={this.handleDisableModal}
-                            handlesave = {this.handleSave}
+                            onSubmit = {this.handleSave}
+                            onCancel = {this.handleCancel}
+                            onChange = {this.handleChange}
+                            selectedexpense = {this.state.selectedExpense}
                         />
                     </div>
                 </Row>
--- src/components/Transaction/New Expense/AddExpenseModal.js
+++ src/components/Transaction/New Expense/AddExpenseModal.js
@@ -2,49 +2,67 @@ import React from 'react';
 import { Modal, Button, Form } from 'react-bootstrap';
 
 const AddExpenseModal = props => {
-    return (
-        <Modal
-            {...props}
-            size="lg"
-            aria-labelledby="contained-modal-title-vcenter"
-            centered
-        >
-            <Modal.Header closeButton>
-                <Modal.Title>Add a Purchase</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form.Group controlId="formItem">
-                    <Form.Label>Item</Form.Label>
-                    <Form.Control type="text" />
-                </Form.Group>
-                <Form.Group controlId="formPrice">
-                    <Form.Label>Price</Form.Label>
-                    <Form.Control type="text" placeholder="$" />
-                </Form.Group>
-                <Form.Group controlId="formCategory">
-                    <Form.Label>Category</Form.Label>
-                    <Form.Control as="select">
-                        <option>Rent</option>
-                        <option>Food</option>
-                        <option>Social</option>
-                        <option>Medical</option>
-                        <option>Transportation</option>
-                        <option>Personal Care</option>
-                    </Form.Control>
-                </Form.Group>
-            </Modal.Body>
-             <Modal.Footer>
-                <Button variant="secondary" onClick={props.onHide}>
-                    Close
-                </Button>
-                <Button variant="primary" onClick={props.handlesave}>
-                    Save Changes
-                </Button>
-            </Modal.Footer>
-        </Modal>
-
-
-    )
-}
+    if (props.selectedexpense) {
+        return (
+            <Modal
+                {...props}
+                size="lg"
+                aria-labelledby="contained-modal-title-vcenter"
+                centered
+            >
+                <Modal.Header closeButton>
+                    <Modal.Title>Add a Purchase</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Form.Group controlId="formItem">
+                        <Form.Label>Item</Form.Label>
+                        <Form.Control 
+                            type="text"
+                            name="item"
+                            value={props.selectedexpense.item}
+                            onChange = {props.onChange} 
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="formPrice">
+                        <Form.Label>Price</Form.Label>
+                        <Form.Control 
+                            type="number" 
+                            name="price"
+                            placeholder="$"
+                            value={props.selectedexpense.price} 
+                            onChange={props.onChange}
+                        />
+                    </Form.Group>
+                    <Form.Group controlId="formCategory">
+                        <Form.Label>Category</Form.Label>
+                        <Form.Control 
+                            as="select"
+                            name="category"
+                            value = {props.selectedexpense.category}
+                            onChange={props.onChange}
+                        >
+                            <option>Rent</option>
+                            <option>Food</option>
+                            <option>Social</option>
+                            <option>Medical</option>
+                            <option>Transportation</option>
+                            <option>Personal Care</option>
+                        </Form.Control>
+                    </Form.Group>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={props.onHide}>
+                        Close
+                    </Button>
+                    <Button variant="primary" onClick={props.onSubmit}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        )
+    } else {
+        return <div/>;
+    }
+};
 
 export default AddExpenseModal;
