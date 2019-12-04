9a3deab62489536aff2bf040b61a4f51883fcb32
Anna Ruble <rublan01@annas-mbp.lc.luther.edu>
2019-12-03 18:54:40 -0600

    fixing getTotals bug

--- src/components/Totals.js
+++ src/components/Totals.js
@@ -21,9 +21,12 @@ class Totals extends Component {
             .then(json => {
                 if (json.success){
                     this.setState({ userId: json.userId, error: false })
-                    purchaseAPI.getTotalsAll(this.state.userId).then(allTotals => this.setState({allTotals: allTotals}))
-                    console.log(this.state.allTotals);
-                    
+                    purchaseAPI.getTotalsAll(this.state.userId).then(allTotals => {
+                        (allTotals).forEach(function(item){
+                            item.totals = ((item.totals/100).toFixed(2));
+                        })
+                        this.setState({allTotals: allTotals});
+                    })
                 } else {
                     // handle error
                     console.log('not working');
@@ -35,11 +38,8 @@ class Totals extends Component {
 
     getTotalsAll() {
         console.log(this.state.allTotals);
-        (this.state.allTotals).forEach(function(item){
-            item.totals = ((item.totals/100).toFixed(2));
-        })
-        console.log(this.state.allTotals);
     }
+    
     render() {
         return (
         <div>
