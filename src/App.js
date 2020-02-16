2266ad9b9604daf4d221a16b6fe32a1937358b54
Anna Ruble <rublan01@annas-mbp.lc.luther.edu>
2020-02-16 13:23:26 -0600

    switch to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route, HashRouter } from 'react-router-dom';
 import { ProtectedRoute } from "./components/Authentication/ProtectedRoute";
 
 import Login from './components//Authentication/Login';
@@ -14,14 +14,14 @@ class App extends Component {
 
   render() {
     return (  
-        <BrowserRouter>
+        <HashRouter>
           <Route exact path="/" component={ Login }/>
           <Route exact path="/signup" component={ Signup }/>
           <ProtectedRoute exact path="/transactions" component={ Transactions }/>
           <ProtectedRoute exact path="/dashboard" component={ Dashboard }/>
           <ProtectedRoute exact path="/goal-mgr" component={ GoalManager }/>
           <ProtectedRoute exact path="/income-mgr" component={ IncomeManager }/>
-        </BrowserRouter>
+        </HashRouter>
     );
   }
 }
