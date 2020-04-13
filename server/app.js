252b1f70bba02d857069d29ce49f846112535750
kuehma02 <kuehma02@luther.edu>
2020-04-13 16:30:13 -0500

    last small fix

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-// const logger = require('morgan');
+const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const favicon = require('serve-favicon');
@@ -11,7 +11,10 @@ const app = express();
 
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, '../public', 'favicon.ico')));
-// app.use(logger('dev'));
+if (process.env.NODE_ENV === 'dev') {
+  app.use(logger('dev'));
+}
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
