4728538073c3fe38c2e16402cb4ce78ba5b466d8
kuehma02 <kuehma02@luther.edu>
2020-04-14 16:42:26 -0500

    Goal page updates

--- src/components/Pages/GoalManager.js
+++ src/components/Pages/GoalManager.js
@@ -2,7 +2,8 @@ import React, {
   Component
 } from 'react';
 import {
-  Container
+  Container,
+  Card
 } from 'react-bootstrap';
 import NavBar from '../Navbar';
 import AddGoal from '../Goals/AddGoal';
@@ -28,11 +29,26 @@ class GoalManager extends Component {
         <NavBar />
         <Container>
           <br />
-          <h1>Manage Goals</h1>
+          <h1>Goals</h1>
+          <Card body>
+            <p>
+              Financial goals are personal objectives you can set for saving and spending money.
+            </p>
+            <p>
+              We at
+              {' '}
+              <b>ExpenseElephant</b>
+              {' '}
+              think that by organizing your money now, you&apos;ll have more for later.
+            </p>
+          </Card>
+          <p />
           <br />
-          <AddGoal
-            stateChange={this.rerender}
-          />
+          <div>
+            <AddGoal
+              stateChange={this.rerender}
+            />
+          </div>
           <br />
           <Goals
             render={this.state.render}
